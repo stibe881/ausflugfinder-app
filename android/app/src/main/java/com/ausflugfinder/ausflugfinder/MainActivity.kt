075b97324c401a6d7ce65b4c0e6db410397b4dc3
stibe881 <stefan.gross@hotmail.ch>
2026-01-13 14:50:01 +0100

    Fix package name in native Android files

--- android/app/src/main/java/com/ausflugfinder/ausflugfinder/MainActivity.kt
+++ android/app/src/main/java/com/ausflugfinder/ausflugfinder/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ausflugfinder.ausflugfinder
+package com.ausflugfinder.app
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/ausflugfinder/ausflugfinder/MainApplication.kt
+++ android/app/src/main/java/com/ausflugfinder/ausflugfinder/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.ausflugfinder.ausflugfinder
+package com.ausflugfinder.app
 
 import android.app.Application
 import android.content.res.Configuration
